761b66c893975f00cfc8155509a79d7c04822113
Emilia Miller <bugitwhisperer@gmail.com>
2023-10-21 00:40:23 +0200

    tests: add tests for pin postal index number page

--- src/pages/pin-postal-index-number.page.ts
+++ src/pages/pin-postal-index-number.page.ts
@@ -3,10 +3,16 @@ import { BasePage } from './base.page';
 import { TopMenuComponent } from '../components/top-menu.component';
 
 export class PostalIndexNumberPage extends BasePage {
-  url = '/postal-code.html';
+  url = '/CS-Central/Code/postal-code.html';
   topMenu = new TopMenuComponent(this.page);
 
   constructor(page: Page) {
     super(page);
   }
+
+  searchBy = this.page.locator('#search_Postal_type');
+  postalInput = this.page.locator('#postal_Input');
+  searchPostOfficeButton = this.page.getByRole('button', { name: 'Search' });
+  numberOfResultsFound = this.page.locator('#postal_list_heading');
+  noResultsFound = this.page.locator('#postal_error');
 }

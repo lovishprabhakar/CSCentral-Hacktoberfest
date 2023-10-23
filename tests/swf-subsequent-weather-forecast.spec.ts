6a4bddc40f9ce26437071bca29966e36bf3807c7
Emilia Miller <bugitwhisperer@gmail.com>
2023-10-23 21:29:15 +0200

    Issue#145/tests: add tests for swf subsequent weather forecast page

---
+++ tests/swf-subsequent-weather-forecast.spec.ts
@@ -0,0 +1,40 @@
+import { test, expect } from '@playwright/test';
+import { SubsequentWeatherForecastPage } from '../src/pages/swf-subsequent-weather-forecast.page';
+
+test.describe('Check weather forecast on SWF - Subsequent Weather Forecast page', () => {
+  let swfPage: SubsequentWeatherForecastPage;
+
+  test.beforeEach(async ({ page }) => {
+    swfPage = new SubsequentWeatherForecastPage(page);
+    await swfPage.goto();
+  });
+
+  test('main-logo navigates to homepage', async ({ page }) => {
+    // Arrange
+    const expectedHomepageURL =
+      'https://lovishprabhakar.is-a.dev/CS-Central/Code/index.html';
+
+    // Act
+    await swfPage.topMenu.homePageLogo.click();
+
+    // Assert
+    expect(page.url()).toBe(expectedHomepageURL);
+  });
+
+  test('check current weather in city', async ({ page }) => {
+    // Arrange
+    const expectedWeatherURL =
+      'https://lovishprabhakar.is-a.dev/CS-Central/Code/currentweather.html';
+
+    // Act
+    await swfPage.searchPlacesButton.click();
+    await swfPage.cityInput.fill('Warsaw, Poland');
+    await swfPage.cityInput.press('Enter');
+    await swfPage.waitForLoadState();
+
+    // Assert
+    expect(page.url()).toBe(expectedWeatherURL);
+    expect(swfPage.foundLocationDetails).toHaveText('Warsaw, PL');
+    expect(swfPage.foundWeatherDetails).toContainText('°C');
+  });
+});

c72714bf64e0c9eb02cdc2390636b7a325625963
Emilia Miller <bugitwhisperer@gmail.com>
2023-10-29 16:46:23 +0100

    refactor(tests): homePage - aggregate atomic actions into navigateToPage() method

--- tests/homepage.spec.ts
+++ tests/homepage.spec.ts
@@ -1,10 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { HomePage } from '../src/pages/home.page';
-import { CodeCompileRunPage } from '../src/pages/ccr-code-compile-run.page';
-import { CurrencyExchangePage } from '../src/pages/cer-currency-exchange.page';
-import { PostalIndexNumberPage } from '../src/pages/pin-postal-index-number.page';
 
-test.describe('Verify top-main menu navigates to expected pages', () => {
+test.describe('Verify top-main menu buttons work as expected, and footer looks as expected', () => {
   let homePage: HomePage;
 
   test.beforeEach(async ({ page }) => {
@@ -12,56 +9,36 @@ test.describe('Verify top-main menu navigates to expected pages', () => {
     await homePage.goto();
   });
 
-  test('ccr navigates to code compile run page', async ({ page }) => {
+  test('ccr button navigates to code compile run page', async ({ page }) => {
     // Act
-    await homePage.topMenu.ccrButton.click();
-    const ccrPage = new CodeCompileRunPage(page);
-    await ccrPage.waitForPageToLoadURL();
+    await homePage.navigateToPage('ccrPage');
 
     // Assert
     expect(await page.textContent('h1')).toBe('CS Central');
     expect(await page.textContent('h3')).toBe(' CCR - Code Compile Run');
   });
 
-  test('cer navigates to currency exchange page', async ({ page }) => {
+  test('cer button navigates to currency exchange page', async ({ page }) => {
     // Act
-    await homePage.topMenu.cerButton.click();
-    const cerPage = new CurrencyExchangePage(page);
-    await cerPage.waitForPageToLoadURL();
+    await homePage.navigateToPage('cerPage');
 
     // Assert
     expect(await page.textContent('h1')).toBe('CS Central');
     expect(await page.textContent('h3')).toBe(' CER - Currency Exchange Rate');
   });
 
-  test('pin navigates to postal index number page', async ({ page }) => {
+  test('pin button navigates to postal index number page', async ({ page }) => {
     // Act
-    await homePage.topMenu.pinButton.click();
-    const pinPage = new PostalIndexNumberPage(page);
-    await pinPage.waitForPageToLoadURL();
+    await homePage.navigateToPage('pinPage');
 
     // Assert
     expect(await page.textContent('h1')).toBe('CS Central');
     expect(await page.textContent('h3')).toBe(' PIN - Post Index Number');
   });
-});
 
-test.describe('Verify headers in footer', () => {
-  let homePage: HomePage;
-
-  test.beforeEach(async ({ page }) => {
-    homePage = new HomePage(page);
-    await homePage.goto();
-  });
-
-  test('footer includes About Us, Contact Me sections', async ({ page }) => {
-    // Arrange
-    const headerInFooter = page.locator('h6');
-    const firstHeader = headerInFooter.first();
-    const lastHeader = headerInFooter.last();
-
+  test('footer includes About Us, Contact Me sections', async () => {
     // Assert
-    await expect.soft(firstHeader).toHaveText('About Us');
-    await expect(lastHeader).toHaveText(' Contact Me');
+    await expect.soft(homePage.footer.firstHeader).toHaveText('About Us');
+    await expect(homePage.footer.lastHeader).toHaveText(' Contact Me');
   });
 });

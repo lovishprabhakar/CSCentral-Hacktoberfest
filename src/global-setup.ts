ff341baeb9861b6963f0e4336da9dba92cfd4c9c
Emilia Miller <bugitwhisperer@gmail.com>
2023-11-29 15:05:31 +0100

    style(global-setup): fix formatting

--- src/global-setup.ts
+++ src/global-setup.ts
@@ -4,9 +4,9 @@ async function globalSetup(): Promise<void> {
   dotenv.config({ override: true });
 
   if (!process.env.local) {
-    console.log('⚠️  URL:', process.env.PROD_BASE_URL)
+    console.log('⚠️  URL:', process.env.PROD_BASE_URL);
   } else {
-    console.log('⚠️  URL:', process.env.LOCAL_BASE_URL)
+    console.log('⚠️  URL:', process.env.LOCAL_BASE_URL);
   }
 }
 
